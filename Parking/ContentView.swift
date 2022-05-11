f91d6f3c93e034a01c8ffc6dadaedc80114bd827
SabbirHossain07 <sabbirsopnil07@gmail.com>
2022-05-11 17:06:25 +0600

    Initial Commit

---
+++ Parking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Parking
+//
+//  Created by Sopnil Sohan on 11/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Parking/ParkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkingApp.swift
+//  Parking
+//
+//  Created by Sopnil Sohan on 11/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ParkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
